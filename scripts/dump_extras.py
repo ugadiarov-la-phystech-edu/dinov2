01117e555cdc2038784539b8e44fb6d8e132d2e6
Leonid Ugadiarov <ugadiarov.la@phystech.edu>
2024-08-25 19:39:17 +0300

    Updated dump_extras.py script to support various datasets

--- scripts/dump_extras.py
+++ scripts/dump_extras.py
@@ -1,14 +1,18 @@
 from argparse import ArgumentParser
-from dinov2.data.datasets import MiniImageNet 
+import dinov2.data.datasets
 
 parser = ArgumentParser()
+parser.add_argument('--dataset_class', type=str, required=True)
+parser.add_argument('--root_path', type=str, required=True)
+parser.add_argument('--extra_path', type=str, required=True)
 
 
-
-def main():
-    for split in MiniImageNet.Split:
-        dataset = MiniImageNet(split=split, root="/code/data/imagenet-mini", extra="/code/data/imagenet-mini/extra")
+def main(args):
+    dataset_class = getattr(dinov2.data.datasets, args.dataset_class)
+    for split in dataset_class.Split:
+        dataset = dataset_class(split=split, root=args.root_path, extra=args.extra_path)
         dataset.dump_extra()
-        
+
+
 if __name__ == "__main__":
-    main()
+    main(parser.parse_args())
